1eab60102378a7aa5a345ff826a027ebbd8450cd
Ks <naga.sathvika.ks@accenture.com>
2025-06-01 21:14:35 +0530

    Add Best_time_to_buy_and_sell_stock.java

---
+++ Best_time_to_buy_and_sell_stock.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+    int a=Integer.MAX_VALUE;
+
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i]<a);
+            {
+                a=prices[i];
+            }
+        }
+    }
+}
